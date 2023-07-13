3eb8488520c58d8f6b18e6d945085eeaaa487b3f
Scott Morrison <scott.morrison@gmail.com>
2023-07-13 16:04:05 +1000

    fix imports

--- Mathlib/Data/List/EditDistance/Defs.lean
+++ Mathlib/Data/List/EditDistance/Defs.lean
@@ -3,11 +3,7 @@ Copyright (c) 2023 Kim Liesinger. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Liesinger
 -/
-import Mathlib.Order.CompleteLattice
-import Mathlib.Order.ConditionallyCompleteLattice.Basic
-import Mathlib.Data.Nat.Lattice
-import Mathlib.Data.List.MinMax
-import Mathlib.Tactic.Abel
+import Mathlib.Data.Nat.Order.Basic
 
 /-!
 # Levenshtein distances
